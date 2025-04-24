58a1692e32921a9fa206818750ab8aab5537d7fc
SamuelRiveira <samuelriveiraescudero@gmail.com>
2025-04-24 12:07:18 +0100

    Uso de Request y GridRepository LoginController

--- src/DungeonTreasureHunt/Server/Backend/controllers/LoginController.php
+++ src/DungeonTreasureHunt/Server/Backend/controllers/LoginController.php
@@ -5,28 +5,30 @@ namespace DungeonTreasureHunt\Backend\controllers;
 use DungeonTreasureHunt\Backend\services\JwtHandler;
 use DungeonTreasureHunt\Backend\services\Response;
 use DungeonTreasureHunt\Backend\http\JsonResponseBuilder;
+use DungeonTreasureHunt\Backend\http\Request;
 
 require_once __DIR__ . '/../services/Response.php';
 require_once __DIR__ . '/../services/JWT.php';
 require_once __DIR__ . '/../http/JsonResponseBuilder.php';
+require_once __DIR__ . '/../http/Request.php';
 
 class LoginController
 {
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
         $users = [
             "admin" => "1234",
             "user" => "abcd"
         ];
 
-        $input = json_decode(file_get_contents("php://input"), true);
+        $body = $request->getBody();
 
-        if (!isset($input['username'], $input['password'])) {
+        if (!isset($body['username'], $body['password'])) {
             return JsonResponseBuilder::error("Faltan datos", 400);
         }
 
-        $username = $input['username'];
-        $password = $input['password'];
+        $username = $body['username'];
+        $password = $body['password'];
 
         if (!isset($users[$username]) || $users[$username] !== $password) {
             return JsonResponseBuilder::error("Credenciales incorrectas", 401);
