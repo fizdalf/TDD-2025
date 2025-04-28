fe294f589c13d1642da63af4d48731484b9fecaf
SamuelRiveira <samuelriveiraescudero@gmail.com>
2025-04-28 13:32:30 +0100

    Solucionado error por import

--- src/DungeonTreasureHunt/Server/index.php
+++ src/DungeonTreasureHunt/Server/index.php
@@ -1,14 +1,15 @@
 <?php
 
+
 use DungeonTreasureHunt\Backend\http\JsonResponseBuilder;
 use DungeonTreasureHunt\Backend\http\Request;
 use DungeonTreasureHunt\Backend\services\Response;
 
 
-
+require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/routes.php';
 
 ini_set('html_errors', false);
-
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
