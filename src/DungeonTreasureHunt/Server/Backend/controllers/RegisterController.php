c78f8a9273d2bb298bc6872f94c0b79b81e0ec6e
SamuelRiveira <samuelriveiraescudero@gmail.com>
2025-05-07 10:19:41 +0100

    Creado RegisterController

---
+++ src/DungeonTreasureHunt/Server/Backend/controllers/RegisterController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace DungeonTreasureHunt\Backend\controllers;
+
+use DungeonTreasureHunt\Framework\http\ApiResponse;
+use DungeonTreasureHunt\Framework\http\Request;
+use DungeonTreasureHunt\Framework\http\Response;
+use DungeonTreasureHunt\Framework\services\UserRepository;
+
+class RegisterController
+{
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $credentials = $this->extractCredentials($request);
+
+        if (!$this->areCredentialsComplete($credentials)) {
+            return ApiResponse::error("Faltan datos", 400);
+        }
+
+        $username = $credentials['username'];
+        $password = $credentials['password'];
+
+        if ($this->userRepository->userExists($username)) {
+            return ApiResponse::error("El usuario ya existe", 409);
+        }
+
+        try {
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $this->userRepository->saveUser($username, $hashedPassword);
+        } catch (\Exception $e) {
+            return ApiResponse::error("Error del servidor: " . $e->getMessage(), 500);
+        }
+
+        return ApiResponse::success(["message" => "Usuario registrado con éxito"], 201);
+    }
+
+    private function extractCredentials(Request $request): array
+    {
+        return $request->parseBodyAsJson();
+    }
+
+    private function areCredentialsComplete(array $credentials): bool
+    {
+        return isset($credentials['username'], $credentials['password']);
+    }
+}
