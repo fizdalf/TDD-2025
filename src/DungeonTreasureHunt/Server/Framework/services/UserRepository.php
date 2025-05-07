363dcbb945146af80be380165f4129ea3c34718f
SamuelRiveira <samuelriveiraescudero@gmail.com>
2025-05-07 10:19:25 +0100

    Creado UserRepository

---
+++ src/DungeonTreasureHunt/Server/Framework/services/UserRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DungeonTreasureHunt\Framework\services;
+
+class UserRepository
+{
+    private string $path;
+
+    public function __construct(string $path = null)
+    {
+        $this->path = $path ?? __DIR__ . '/../../Backend/data/users.txt';
+    }
+
+    public function getUsers(): array
+    {
+        if (!file_exists($this->path)) {
+            return [];
+        }
+
+        return json_decode(file_get_contents($this->path), true) ?? [];
+    }
+
+    public function saveUser(string $username, string $hashedPassword): void
+    {
+        $users = $this->getUsers();
+        $users[$username] = $hashedPassword;
+        file_put_contents($this->path, json_encode($users, JSON_PRETTY_PRINT));
+    }
+
+    public function userExists(string $username): bool
+    {
+        $users = $this->getUsers();
+        return isset($users[$username]);
+    }
+}
